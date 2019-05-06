3f8efda58eea51ed6c142f6b4efe828b4ea1864a
nam2934 <nam2934@naver.com>
2019-05-06 22:16:04 +0900

    make post and write function

--- 5weeks_AS/AS_5/urls.py
+++ 5weeks_AS/AS_5/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from blog import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.home, name='home'),
+    path('<int:blog_id>/', views.detail, name='detail'),
+    path('new/', views.new, name='new'),
+    path('create/', views.create, name='create'),
 ]
--- 5weeks_AS/blog/views.py
+++ 5weeks_AS/blog/views.py
@@ -1,7 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
 
 from .models import Blog
 # Create your views here.
 
 def home(request):
-    return render(request, 'blog/home.html')
+    blogs = Blog.objects
+    return render(request, 'blog/home.html', {'blogs':blogs})
+
+def detail(request, blog_id):
+    blog_detail = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'blog/detail.html', {'blog': blog_detail})
+
+def new(request):
+    return render(request, 'blog/new.html')
+
+def create(requset):
+    blog = Blog()
+    blog.title = requset.GET['title']
+    blog.body = requset.GET['body']
+    blog.pub_date = timezone.datetime.now()
+    blog.save()
+    return redirect('/'+str(blog.id))
+
+
